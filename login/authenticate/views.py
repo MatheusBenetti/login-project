1bc2f6546a2e0e7d34165ee02b1a3f558526e9db
MatheusBenetti <mbbenetti1@gmail.com>
2022-09-15 16:21:00 -0300

    signup and login functions

--- login/authenticate/views.py
+++ login/authenticate/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -6,10 +9,40 @@ def home(request):
 
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password1')
+        confirm_password = request.POST.get('password2')
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+        mail = request.POST.get('mail')
+
+        new_user = User.objects.create_user(username, mail, password=password)
+        new_user.first_name = first_name
+        new_user.last_name = last_name
+        new_user.save()
+
+        messages.success(request, 'Your account has been successfully created.')
+
+        return redirect('login')
+
     return render(request, 'auth/signup.html')
 
 
-def login(request):
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'auth/home.html')
+        else:
+            messages.error(request, 'Bad credentials.')
+            return redirect('home')
+
     return render(request, 'auth/login.html')
 
 
