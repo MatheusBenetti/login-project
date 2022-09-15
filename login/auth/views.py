b0309f3fd7dbf1b70fa52fee264386ab38a4fca6
MatheusBenetti <mbbenetti1@gmail.com>
2022-09-15 11:16:41 -0300

    created some functions

---
+++ login/auth/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'auth/index.html')
+
+
+def signup(request):
+    return render(request, 'auth/signup.html')
+
+
+def login(request):
+    return render(request, 'auth/login.html')
+
+
+def logout(request):
+    pass
